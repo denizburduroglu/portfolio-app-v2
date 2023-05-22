a4e59fe76cc7f2fcba552d914958eb4899fd0ac4
Deniz Burduroglu <denizburduroglu@gmail.com>
2023-05-22 15:29:54 -0700

    pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,24 @@ import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({ registerType: 'autoUpdate' })
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'robots.txt'],
+      manifest: {
+        name: 'Your App Name',
+        short_name: 'App Name',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          // Add your app's icons here
+        ],
+      },
+      workbox: {
+        // Configuration for Workbox
+      },
+    })
   ],
   base: '/portfolio-app-v2/'
 })
